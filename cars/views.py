18af8c9dcfefea2574dd211cddafd85a0cb6dd30
Caio Hisamatsu <caiomizohata@gmail.com>
2025-08-05 20:18:10 -0300

    first commit

---
+++ cars/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from cars.models import Car
+from cars.forms import CarModelForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+
+class CarListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+    ordering = ['-value']
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
+
+
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['new_cars_forms'] = CarModelForm()
+        return context
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+    context_object_name = 'car'
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/'
